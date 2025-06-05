7c82fa29994cdd8a967c5359a9b7e73509439ee6
hobaek <wogh104@naver.com>
2025-06-05 16:47:58 +0900

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ import { execSync } from 'child_process';
 
 const app = express();
 app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.use(express.json());
 
 const getChromePath = () => {
